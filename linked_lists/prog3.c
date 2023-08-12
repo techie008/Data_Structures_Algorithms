f78c0bbff38ba8c8d4331f3cec3fcb63470b2b6c
techie008 <142090038+techie008@users.noreply.github.com>
2023-08-12 19:09:41 +0530

    Add files via upload

---
+++ linked_lists/prog3.c
@@ -0,0 +1,65 @@
+//write a program to reverse a linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+        int data;
+        struct node *next;
+};
+
+struct node *head=NULL;
+
+void insert_node(int data){
+        struct node *new_node=(struct node *)malloc(sizeof(struct node));
+        new_node->data=data;
+        new_node->next=NULL;
+
+        if(head==NULL){
+                new_node->next=NULL;
+                head=new_node;
+        }
+
+        else{
+                struct node *temp=head;
+                while(temp->next!=NULL){
+                        temp=temp->next;
+                }
+                temp->next=new_node;//inserting at last
+        }
+}
+
+void reverse_ll(){
+	struct node *current=head;
+	struct node *prev=NULL;
+	struct node *next=NULL;
+	
+	while(current!=NULL){
+		next=current->next;
+		current->next=prev;
+		prev=current;
+		current=next;
+	}
+	head=prev;
+}
+
+void display(){
+	struct node *temp=head;
+	while(temp!=NULL){
+		printf("%d ",temp->data);
+		temp=temp->next;
+	}
+}
+
+int main(){
+        int n;
+        scanf("%d",&n);
+        int item;
+        while(n--){
+                scanf("%d",&item);
+                insert_node(item);
+        }
+        reverse_ll();
+	display();
+        return 0;
+}
---
+++ linked_lists/prog5.c
@@ -0,0 +1,66 @@
+//detect loop in a linked list
+//the question demands us to detect loop if found
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+        int data;
+        struct node *next;
+	int flag=0; //if flag=1 means node has been visited once, revisiting would mean loop
+};
+
+struct node *head=NULL;
+
+void insert_node(int data){
+        struct node *new_node=(struct node *)malloc(sizeof(struct node));
+        new_node->data=data;
+        new_node->next=NULL;
+
+        if(head==NULL){
+                new_node->next=NULL;
+                head=new_node;
+        }
+
+        else{
+                struct node *temp=head;
+                while(temp->next!=NULL){
+                        temp=temp->next;
+                }
+                temp->next=new_node;//inserting at last
+        }
+}
+bool detect_cycle(){
+	struct node *temp=head;
+	while(temp!=NULL){
+		if(temp->flag==0){
+			temp->flag=1;
+		}
+		else
+			return true;
+		temp=temp->next;
+	}
+	return false;
+}
+
+int main(){
+        int n;
+        scanf("%d",&n);
+        int item;
+        while(n--){
+                scanf("%d",&item);
+                insert_node(item);
+        }
+	//create a loop
+	
+	struct node *temp=head;
+	while(temp->next!=NULL){
+		temp=temp->next;
+	}
+	temp->next=head;
+	
+        if(detect_cycle())
+		printf("Cycle detected!");
+	else
+		printf("no loops detected!");
+        return 0;
+}
