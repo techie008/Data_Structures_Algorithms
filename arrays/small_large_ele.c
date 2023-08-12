e58e0f55973ed285dffea23c4fc99cd2daec78f0
techie008 <142090038+techie008@users.noreply.github.com>
2023-08-12 19:03:44 +0530

    Create small_large_ele.c

---
+++ arrays/small_large_ele.c
@@ -0,0 +1,39 @@
+//print the smallest and largest element in the array
+//Simple Approach : traverse the array and update the smallest and largest element
+//Time-Complexity : O(n)
+
+//Efficient Approach : Sort the array with a O(logn) sorting algorithm then return the first and last element
+//Time-Complexity : O(logn)
+
+
+#include <stdio.h>
+int main(){
+    int size;
+    int ch;
+    float sum=0;
+    float avg=0;
+    scanf("%d",&size);
+    int array[size];
+    
+    for(int i=0;i<size;i++){
+        scanf("%d",&array[i]);
+    }
+    scanf("%d",&ch);
+    int large=array[0];
+    int small=array[0];
+    
+
+    for(int i=1;i<size;i++){
+        if(array[i]<small)
+            small=array[i];
+    }
+        printf("%d",small);
+    
+  
+    for(int i=1;i<size;i++){
+        if(array[i]>large)
+            large=array[i];
+    }
+        printf("%d",large);
+    
+}
