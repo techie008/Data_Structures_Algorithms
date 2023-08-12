9f0d1f9aafc6d3401ecb1f3e12cc6ae5b9ef57ee
techie008 <142090038+techie008@users.noreply.github.com>
2023-08-12 16:26:39 +0530

    Update kth_distance.c

--- arrays/kth_distance.c
+++ arrays/kth_distance.c
@@ -1,1 +1,37 @@
+/*
+Given an unsorted array that may contain duplicates, check if the given array contains duplicate elements within 'k' distance from each other. 
+Also, given a number 'k' which is smaller than the size of the array, write a program that returns true if the array contains duplicates within 'k' distance.
+*/
+//Difficulty : Medium
+//Time-Complexity : O(n^2)
+
+#include <stdio.h>
+
+int main(){
+    
+    int count;
+    int size;
+    int k,dupl;
+    scanf("%d",&size);
+    int array[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&array[i]);
+    }
+    scanf("%d",&k);
+    
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            if(array[i]==array[j]){
+                dupl=j-i;
+            }
+        }
+    }
+    if(dupl==k){
+        printf("Yes");
+    }
+    else
+        printf("No");
+}
+
+
 
